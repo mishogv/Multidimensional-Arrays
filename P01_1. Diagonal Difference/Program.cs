0176f807ba62e882b76d7f40786beb2b6eef7d2a
Mihail Petrov <m_i_h_o@abv.bg>
2019-01-17 16:35:06 +0200

    Add project files.

---
+++ P01_1. Diagonal Difference/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01_1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
